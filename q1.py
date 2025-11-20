d607e64e18751b20d51ba5945b96e9b0866d889c
Tulsi2006123 <tulsi.cd241110@global.org.in>
2025-11-20 11:07:42 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,11 @@
+import math
+radius = 42
+angle_deg = 60
+
+arc_length = (angle_deg / 360) * 2 * math.pi * radius
+
+side = arc_length /  4
+area = side * side
+
+print("Side of square:", side)
+print("Area of square:",area)
